5fb103fa399812081e36525cd8b58bf5c151d4a3
faraday58 <jarodriguezvera@gmail.com>
2018-05-04 16:46:18 -0500

    Estructura inicial

---
+++ EjemploEstructuras/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EjemploEstructuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public struct miEstructura
+    {
+        private int val1;
+        private int val2;
+
+        public miEstructura(int val1, int val2)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+    }
+}
